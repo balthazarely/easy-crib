564398a3829f78349895fba25ab375621b11f98e
balthazarely <balthazar.elyj@gmail.com>
2023-07-13 08:34:27 -0600

    phone test

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,7 @@ function App() {
   }, [score]);
 
   return (
-    <div className="card-container  ">
-      {isGameOver ? "GAME OVER" : ""}
+    <div className="card-container pt-3 bg-base-300 flex flex-col  ">
       {players.map((player) => (
         <Player
           key={player.id}
@@ -42,8 +41,8 @@ function App() {
           isGameOver={isGameOver}
         />
       ))}
-
-      {/* <ScoreBoard players={players} score={score} setScore={setScore} /> */}
+      {isGameOver ? "GAME OVER" : ""}
+      <ScoreBoard players={players} score={score} setScore={setScore} />
     </div>
   );
 }
@@ -65,47 +64,34 @@ function Player({ player, setScore, score, isGameOver }) {
   };
 
   return (
-    <div className="flex justify-start flex-col items-center bg-base-300  flex-grow w-screen h-1/3">
-      <div className="p-3 w-full flex-grow flex flex-row justify-center items-center ">
-        <div className="w-full py-2 gap-4 bg-base-100  shadow-lg rounded-3xl  justify-center items-center   h-full flex ">
+    <div className="flex justify-start flex-col items-center    flex-grow w-screen h-1/5">
+      <div className="p-1 px-3 w-full flex-grow flex flex-row justify-center items-center ">
+        <div className="w-full  gap-4 bg-base-100  shadow-lg rounded-3xl  justify-center items-center   h-full flex ">
           <div className="  w-1/3 flex h-full flex-col justify-center items-center">
             <div className="text-2xl">{player.name}</div>
             <div className="text-6xl font-bold">
               {score.length > 0 && calcScore(score)}
             </div>
           </div>
-          <div className="btn-group  w-2/3 h-full  flex justify-center items-center gap-2">
-            <button
-              disabled={isGameOver}
-              onClick={() => handleScoreChange(1)}
-              className="btn btn-primary btn-lg h-20 w-24"
-            >
-              + 1
-            </button>
-            <button
-              className="btn btn-primary btn-lg h-20 w-24"
-              onClick={() => handleScoreChange(2)}
-            >
-              + 2
-            </button>
+          <div className=" w-2/3 h-full flex-col flex justify-center items-center gap-2  relative ">
+            <div className=" btn-group">
+              <button
+                disabled={isGameOver}
+                onClick={() => handleScoreChange(1)}
+                className="btn btn-primary btn-lg h-20 w-24"
+              >
+                + 1
+              </button>
+              <button
+                disabled={isGameOver}
+                className="btn btn-primary btn-lg h-20 w-24"
+                onClick={() => handleScoreChange(2)}
+              >
+                + 2
+              </button>
+            </div>
           </div>
         </div>
-        {/* <button
-            disabled={isGameOver}
-            onClick={() => handleScoreChange(1)}
-            className="btn btn-primary w-full"
-          >
-            + 1
-          </button>
-          <button
-            disabled={isGameOver}
-            onClick={() => handleScoreChange(2)}
-            className="btn btn-primary "
-            // className=" w-full h-1/2  rounded-br-3xl bg-green-600 border-none transition-all duration-150 rounded-sm  text-neutral-200 hover:bg-green-800 text-2xl font-bold"
-          >
-            + 2
-          </button> */}
-        {/* </div> */}
       </div>
     </div>
   );
@@ -123,20 +109,24 @@ function ScoreBoard({ players, score, setScore }) {
   };
 
   return (
-    <div>
+    <div className="border-2 h-64 overflow-y-scroll">
       <button
         className=" w-10 bg-green-600 border-none transition-all duration-150 rounded-sm text-white hover:bg-green-800 h-10"
         onClick={undo}
       >
         undo
       </button>
-      {score.map((sc) => {
-        return (
-          <div>
-            {sc.name}: {sc.score}
-          </div>
-        );
-      })}
+      {score
+        .slice(3)
+        .slice(0)
+        .reverse()
+        .map((sc) => {
+          return (
+            <div className="w-full py-2 text-center">
+              {sc.name}: <span className="font-black">+ {sc.score}</span>
+            </div>
+          );
+        })}
     </div>
   );
 }
